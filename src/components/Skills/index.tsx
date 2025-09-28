36aa2a3881c42e9813bbf889906eb56c0b825533
Shrey Gajjar <shreygajjar786@gmail.com>
2025-09-28 17:00:15 +0530

    Created| skills gallery with tools and skills images

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,10 +1,23 @@
+"use client";
 import React from 'react'
+import DomeGallery from './SkillsGallery'
+import { SKILLS_AND_TOOLS } from '../constants/skilsList';
+
+export type ImageItem = string | { src: string; alt?: string };
 
 const Skills = () => {
-    return (
-        <div className='w-full h-full'>
-            skills
+    return (<>
+        <div className='w-full flex justify-center'>
+            <p className='text-4xl w-2/4 font-bold text-[#ffffffcb]'>
+                My Go-To Stack for Building Secure and Scalable Applications
+            </p>
         </div>
+        <div className='w-full h-screen'>
+            <DomeGallery
+                images={SKILLS_AND_TOOLS}
+            />
+        </div>
+    </>
     )
 }
 
