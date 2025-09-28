3b2aabdd0f442d3c2b9c63d0a96fa1cff3414756
Shrey Gajjar <shreygajjar786@gmail.com>
2025-09-28 17:29:58 +0530

    Update| code refactor

---
+++ src/components/constants/skilsList.ts
@@ -0,0 +1,52 @@
+import { ImageItem } from "../Skills";
+
+export const SKILLS_AND_TOOLS: ImageItem[] = [
+    {
+        src: '/skills/solidity.svg',
+        alt: 'Solidity'
+    },
+    {
+        src: '/skills/aws.svg',
+        alt: 'AWS'
+    },
+    {
+        src: '/skills/docker.svg',
+        alt: 'Docker'
+    },
+    {
+        src: '/skills/foundry.avif',
+        alt: 'Foundry'
+    },
+    {
+        src: '/skills/gcp.svg',
+        alt: 'Google Cloud'
+    },
+    {
+        src: '/skills/hardhat.svg',
+        alt: 'hardhat'
+    },
+    {
+        src: '/skills/nestjs.svg',
+        alt: 'NestJS'
+    },
+    {
+        src: '/skills/nextjs.svg',
+        alt: 'NextJS'
+    },
+    {
+        src: '/skills/oracle.svg',
+        alt: 'Oracle'
+    },
+    {
+        src: '/skills/postman.svg',
+        alt: 'Postman'
+    },
+    {
+        src: '/skills/rust.svg',
+        alt: 'Rust'
+    },
+    {
+        src: '/skills/typescript.svg',
+        alt: 'TS'
+    },
+];
